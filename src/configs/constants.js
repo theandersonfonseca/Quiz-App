47a4d30c628a27689b1d228aa520362123cacf90
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-04-06 15:57:01 -0300

    add constants color for theme provider

---
+++ src/configs/constants.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  dark: '#333',
+  gray: 'rgb(105, 104, 104)',
+  white: '#fff',
+  green: '#07f035',
+  greenDark: '#119c2c',
+  blue: 'rgb(54, 112, 199)',
+  red: '#f11a16',
+};
